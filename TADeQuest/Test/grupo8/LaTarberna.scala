ae095ab3e543625d44404ee09d7f139aeeea34f6
AgustinDavidNievas <AgustinDavidNievas90@gmail.com>
2016-06-25 01:40:58 -0300

    Mini refactor + test

--- TADeQuest/Test/grupo8/LaTarberna.scala
+++ TADeQuest/Test/grupo8/LaTarberna.scala
@@ -5,21 +5,73 @@ import org.junit.Test
 import org.junit.Assert._
 
 class LaTarberna extends TAdeQuestPrueba {
-      
+  
+  var equipo: Equipo = null
+  var misionfalla: Mision = null
+  var mision1: Mision = null
+  var mision2: Mision = null
+  var mision3: Mision = null
+  
+  @Before
+  override def setup{
+    super.setup
+    
+    equipo = new Equipo("Los 3 chiflados", List(ashe,coco,pepe))
+    
+    misionfalla = new Mision(Set(nadiePuedeHacerla), {_.adherirAlPozoComun(999999999)})
+    mision1 = new Mision(Set(forzarPuerta), {_.adherirAlPozoComun(200)})
+    mision2 = new Mision(Set(pelearContraMonstruo), {_.adherirAlPozoComun(400)})
+    mision3 = new Mision(Set(new robarTalisman(talismanDedicacion)), {_.adherirAlPozoComun(1000)})
+    
+  }
+  
   @Test
   def mejorMision  {
-    var equipo = new Equipo("Los 3 chiflados", List(ashe,coco,pepe))
     
-    val misionfalla = new Mision(Set(nadiePuedeHacerla), {_.adherirAlPozoComun(999999999)})
-    val mision1 = new Mision(Set(forzarPuerta), {_.adherirAlPozoComun(200)})
-    val mision2 = new Mision(Set(pelearContraMonstruo), {_.adherirAlPozoComun(400)})
-    val mision3 = new Mision(Set(new robarTalisman(talismanDedicacion)), {_.adherirAlPozoComun(1000)})
     // La mision3 no la pueden realizar porque el lider no es un ladrón
     var taberna = new Taberna(new Tablon(Set(misionfalla,mision1,mision2,misionfalla,mision3)))
     
     val mejorMision = taberna.getTablon.elegirMision2(equipo, (e1,e2) => e1.pozoComun > e2.pozoComun)
-
+    
+    
+    assertEquals(0, equipo.pozoComun)//el equipo no sufre cambios
     assertEquals("Pelear contra Monstruo", mejorMision.get.tareas.head.getDescripcion)
-  } 
+  }
+  
+  @Test
+  def heroesEntrenan  {
+      
+    var taberna = new Taberna(new Tablon(Set(mision2,mision1)))
+    
+    equipo = new Equipo("Los 3 chiflados", List(ashe,coco,pepe))
+    
+    equipo = taberna.entrenar(equipo, (e1,e2) => e1.pozoComun > e2.pozoComun)
+    
+    assertEquals(600, equipo.pozoComun)
+  }
+  
+  @Test
+  def heroesNoEntrenanEquipoNoSufreEfecto  {
+      
+    var taberna = new Taberna(new Tablon(Set(misionfalla)))
+    
+    equipo = new Equipo("Los 3 chiflados", List(ashe,coco,pepe))
+    
+    equipo = taberna.entrenar(equipo, (e1,e2) => e1.pozoComun > e2.pozoComun)
+    
+    assertEquals(0, equipo.pozoComun)
+  }
+  
+  @Test
+  def heroesEntrenanListaVaciaNoRompe  {
+      
+    var taberna = new Taberna(new Tablon(Set()))
+    
+    equipo = new Equipo("Magios", List(ashe,coco,pepe))
+    
+    equipo = taberna.entrenar(equipo, (e1,e2) => e1.pozoComun > e2.pozoComun)
+    
+    assertEquals(0, equipo.pozoComun)
+  }
 
 }
--- TADeQuest/src/grupo8/Taberna.scala
+++ TADeQuest/src/grupo8/Taberna.scala
@@ -7,18 +7,20 @@ class Taberna(tablon: Tablon = Tablon(Set())) {
   
   def getTablon = tablon
   
-  def entrenar(equipo: Equipo): Equipo = {
+  def entrenar(equipo: Equipo, criterio: (Equipo,Equipo) => Boolean): Equipo = {
     var restantes = tablon
     var equipoEntrenando = equipo
-    var mision: Mision = ???
     
-    while(tablon.tieneMisiones){
-      mision = tablon.elegirMision(equipo, (e1,e2) => e1.pozoComun > e2.pozoComun).get      
-      equipoEntrenando = equipo.realizarMision(mision) match {
-        case Success(e) => e
-        case Failure(f) => break
-      }
-      restantes = tablon.sacarMision(mision)
+    
+    while(restantes.tieneMisiones){
+      restantes.elegirMision2(equipoEntrenando, criterio) match {
+        case Some(m) => {
+                         equipoEntrenando = equipoEntrenando.realizarMision(m).get;
+                         restantes = restantes.sacarMision(m)
+                         }
+        case None => restantes = new Tablon
+      }      
+      
     }
     
     equipoEntrenando
